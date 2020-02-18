8fa9a1811d225cf320ed713ebfa292847083db4a
Emily <57361805+emilygracekz@users.noreply.github.com>
2020-02-18 20:07:38 +0200

    Add files via upload
    Any option is now fully functioning

--- nomadDiscover.js
+++ nomadDiscover.js
@@ -1,48 +1,68 @@
 // JS modules (es6, see https://exploringjs.com/es6/ch_modules.html)
-import options from './nomadDiscoverDatabase'
+import options from "./nomadDiscoverDatabase";
 //match input to database
 
-let inputContinent = document.querySelector("#continent").value;
+function matchInput() {
+  let inputContinent = document.querySelector("#continent").value;
   let inputBudget = document.querySelector("#budget").value;
   let inputWeather = document.querySelector("#weather").value;
   let inputSize = document.querySelector("#size").value;
 
-
-function matchInput() {
-  // let inputContinent = document.querySelector("#continent").value;
-  // let inputBudget = document.querySelector("#budget").value;
-  // let inputWeather = document.querySelector("#weather").value;
-  // let inputSize = document.querySelector("#size").value;
-
-  //continent filter
+  // continent filter
+  let continentsFiltered;
   const continentFilter = entry => {
     return entry.continent === inputContinent;
   };
-  const continentsFiltered = options.filter(continentFilter);
-  console.log(continentsFiltered);
+
+  const isContinentAny = inputContinent === "Any";
+  if (isContinentAny) {
+    continentsFiltered = options;
+  } else {
+    continentsFiltered = options.filter(continentFilter);
+    console.log(continentsFiltered);
+  }
 
   //budget filter
+  let budgetFiltered;
   const budgetFilter = select => {
     return select.budget <= inputBudget;
   };
-  const budgetFiltered = continentsFiltered.filter(budgetFilter);
-  console.log(budgetFiltered);
+  const isBudgetAny = inputBudget === "Any";
+  if (isBudgetAny) {
+    budgetFiltered = continentsFiltered;
+  } else {
+    budgetFiltered = continentsFiltered.filter(budgetFilter);
+    console.log(budgetFiltered);
+  }
 
   //weather filter
+  let weatherFiltered;
   const weatherFilter = pick => {
     return pick.weather === inputWeather;
   };
-  console.log(weatherFilter);
-  const weatherFiltered = budgetFiltered.filter(weatherFilter);
+  const isWeatherAny = inputWeather === "Any";
+  if (isWeatherAny) {
+    weatherFiltered = budgetFiltered;
+  } else {
+  weatherFiltered = budgetFiltered.filter(weatherFilter);
   console.log(weatherFiltered);
+  }
 
   // size filter
+  let sizeFiltered;
   const sizeFilter = you => {
     return you.size === inputSize;
   };
-  const sizeFiltered = weatherFiltered.filter(sizeFilter);
+  console.log(inputSize);
+  const isSizeAny = inputSize === "Any";
+  if (isSizeAny) {
+    sizeFiltered = weatherFiltered;
+  } else {
+  sizeFiltered = weatherFiltered.filter(sizeFilter);
+  }
   console.log(sizeFiltered);
   return sizeFiltered;
+  
 } //end of matchInputFunction
 
 //grab modal parts
@@ -50,15 +70,22 @@ const modalOuter = document.querySelector(".modal-outer");
 const modalInner = document.querySelector(".modal-inner");
 
 //handle the submission, populate modal
+
 function onSubmit(e) {
   e.preventDefault();
   let finalList = matchInput();
-  if (finalList.length === 0) {
-    modalInner.innerHTML = `<p>Your input didn't match any cities! Maybe you should be less picky 😉</p>`;  
-  
+  if (finalList.length === 0) 
+  {
+  modalInner.innerHTML = `<p>Your input didn't match any cities! Maybe you should be less picky 😉</p>`;
+
   //show modal
   modalOuter.classList.add("open");
+  //reset the form
+  e.target.reset();
+  return;
   }
+
+  e.preventDefault();
 
   //reset the form
   e.target.reset();
@@ -73,27 +100,25 @@ function onSubmit(e) {
 
   //show modal
   modalOuter.classList.add("open");
-
-  //close the model when clicked out or esc is pressed
-  function closeModal() {
-    modalOuter.classList.remove("open");
-  }
-  
-  modalOuter.addEventListener("click", function(event) {
-    const isOutside = !event.target.closest(".modal-inner");
-    if (isOutside) {
-      modalOuter.classList.remove("open");
-    }
-  });
-  
-  window.addEventListener("keydown", event => {
-    if (event.key === "Escape") {
-      closeModal();
-    }
-  });
-  //end of modals 
 }
 
+//close the model when clicked out or esc is pressed
+function closeModal() {
+  modalOuter.classList.remove("open");
+}
+
+modalOuter.addEventListener("click", function(event) {
+  const isOutside = !event.target.closest(".modal-inner");
+  if (isOutside) {
+    modalOuter.classList.remove("open");
+  }
+});
+
+window.addEventListener("keydown", event => {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+});
+//end of modals
+
 document.querySelector("form").addEventListener("submit", onSubmit);
-
-
